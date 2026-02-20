e467a678aa7f1d49c5d6793998b45eeceaab24fd
Marcio Vidal <marciomvm@gmail.com>
2026-02-20 00:26:27 +0000

    feat: Add Supabase API query scripts to test parent relationship fetching and schema retrieval.

---
+++ test-query3.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+fetch("https://wjixkrgakcjxtvhczwyf.supabase.co/rest/v1/mush_batches?select=id,readable_id,parent_id,parent:mush_batches(readable_id)&limit=5&parent_id=not.is.null", {
+    headers: {
+        "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqaXhrcmdha2NqeHR2aGN6d3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwOTUyODIsImV4cCI6MjA4MjY3MTI4Mn0.ScYZZ5DblF35n-_ZnL91gCisQgqO277mtjRk969d5VA",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqaXhrcmdha2NqeHR2aGN6d3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwOTUyODIsImV4cCI6MjA4MjY3MTI4Mn0.ScYZZ5DblF35n-_ZnL91gCisQgqO277mtjRk969d5VA"
+    }
+}).then(res => res.json()).then(data => {
+    fs.writeFileSync('C:\\Users\\Marcio\\Labels\\test-output3.json', JSON.stringify(data, null, 2));
+}).catch(e => console.error(e));
